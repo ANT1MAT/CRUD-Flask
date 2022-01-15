0fe4e55c13bf20f4ec9e1580df8ece42aff78402
Alexey Cheremnyy <antimat42@gmail.com>
2022-01-15 11:46:15 +0300

    Add comments in code

--- app/mainpage/mainpage.py
+++ app/mainpage/mainpage.py
@@ -1,17 +1,18 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-
-from models.wishlist import Wishlist, db
+from app.models.wishlist import Wishlist, db
 
 
 mainpage_blueprint = Blueprint('mainpage', __name__,
                            template_folder='templates/html')
 
 
+#Отображает главную страницу
 @mainpage_blueprint.route('/', methods=['get'])
 def mainpage():
     return render_template('mainpage.html')
 
 
+#Добавляет новую запись в БД
 @mainpage_blueprint.route('/', methods=['post'])
 def additem():
     try:
@@ -24,13 +25,14 @@ def additem():
     return render_template('mainpage.html')
 
 
-
+#Отображает список желаний
 @mainpage_blueprint.route('/mylist', methods=['get'])
 def mywishlist():
     list=Wishlist.query.all()
     return render_template('wishlist.html', list=list)
 
 
+#Удаляет запись о выбранном желании из БД
 @mainpage_blueprint.route('/mylist', methods=['post'])
 def deleteitem():
     id_item = request.form['item']
